e3cae950c220223888f98518277281955391f08a
Jaren Whitehouse <jarenwhitehouse@gmail.com>
2024-12-21 09:34:50 +0000

    Save progress: 950.1

--- bingo.sh
+++ bingo.sh
@@ -2,10 +2,7 @@
 # Bingo Number Generator
 echo -e "\n~~ Bingo Number Generator ~~\n"
 NUMBER=$(( RANDOM % 75 + 1 ))
-echo $NUMBER
 TEXT="The next number is, "
-if (( NUMBER <= 15 ))
-then
-then
+if (( NUMBER <= 15 )); then
   echo $TEXT B:$NUMBER
 fi
