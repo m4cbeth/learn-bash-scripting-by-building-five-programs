78c8679de718e768c5b01850b4e75f250fd9fcd2
Jaren Whitehouse <jarenwhitehouse@gmail.com>
2024-12-21 10:22:02 +0000

    Save progress: 1412.1

--- .freeCodeCamp/test.sh
+++ .freeCodeCamp/test.sh
@@ -5,8 +5,17 @@ RESPONSES=("Yes" "No" "Maybe" "Outlook good" "Don't count on it" "Ask again late
 N=$(( RANDOM % 6 ))
 # echo ${RESPONSES[$N]}
 GET_FORTUNE() {
+  if [[ ! $1 ]]
+  then
   echo Ask a yes or no question:
+  else
+  echo Try again. Make sure it ends with a question mark:
+  fi 
   read QUESTION
+
 }
+until [[  $QUESTION =~ \?$ ]]
+do
 GET_FORTUNE
-echo $QUESTION
+done
+GET_FORTUNE again
