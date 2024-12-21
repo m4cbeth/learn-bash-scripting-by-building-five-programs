1a923331383174098533364dc375280374d1a387
Jaren Whitehouse <jarenwhitehouse@gmail.com>
2024-12-21 09:57:42 +0000

    Save progress: 1140.1

--- .freeCodeCamp/test.sh
+++ .freeCodeCamp/test.sh
@@ -2,5 +2,6 @@
 # Program to tell a persons fortune
 echo -e "\n~~ Fortune Teller ~~\n"
 RESPONSES=("Yes" "No" "Maybe" "Outlook good" "Don't count on it" "Ask again later" )
-echo {{ $RESPONSES[0] }}
-declare -p RESPONSES
+echo  ${RESPONSES[5]}
+N=$(( RANDOM % 6 ))
+echo $N
