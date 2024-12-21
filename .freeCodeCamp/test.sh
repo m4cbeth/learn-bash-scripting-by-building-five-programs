26d1362a87d6707fd07f46a8c04ad861e918af61
Jaren Whitehouse <jarenwhitehouse@gmail.com>
2024-12-21 10:02:57 +0000

    Save progress: 1220.1

--- .freeCodeCamp/test.sh
+++ .freeCodeCamp/test.sh
@@ -3,9 +3,10 @@
 echo -e "\n~~ Fortune Teller ~~\n"
 RESPONSES=("Yes" "No" "Maybe" "Outlook good" "Don't count on it" "Ask again later" )
 N=$(( RANDOM % 6 ))
-echo ${RESPONSES[$N]}
+# echo ${RESPONSES[$N]}
 GET_FORTUNE() {
   echo Ask a yes or no question:
+  read QUESTION
 }
-
 GET_FORTUNE
+echo $QUESTION
