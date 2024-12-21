5205b48d8f9eb764faa1f7214cf98bf651c15b12
Jaren Whitehouse <jarenwhitehouse@gmail.com>
2024-12-21 11:01:14 +0000

    break time

--- .freeCodeCamp/test.sh
+++ .freeCodeCamp/test.sh
@@ -1,6 +1,2 @@
 #!/bin/bash
 #Program to run my other four programs
-./questionnaire.sh
-./countdown.sh a
-./bingo.sh
-./fortune.sh
--- five.sh
+++ five.sh
@@ -1,6 +1,2 @@
 #!/bin/bash
 #Program to run my other four programs
-./questionnaire.sh
-./countdown.sh 3
-./bingo.sh
-./fortune.sh
